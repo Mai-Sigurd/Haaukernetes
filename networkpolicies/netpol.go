5253ab15d46254b1bd9a54682e766366ca654837
kaep <sebastianholmgaard@live.dk>
2023-03-06 10:11:45 +0100

    progress implementing network policies - handling of labels and namespaces (maybe more) is not yet decided, so this is very much a work in progress

--- networkpolicies/netpol.go
+++ networkpolicies/netpol.go
@@ -7,30 +7,73 @@ import (
 
 	//appsv1 "k8s.io/api/apps/v1"
 	//apiv1 "k8s.io/api/core/v1"
+	v1 "k8s.io/api/core/v1"
 	network "k8s.io/api/networking/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/intstr"
 	"k8s.io/client-go/kubernetes"
 	//networkv1 "k8s.io/client-go/kubernetes/typed/networking/v1"
 )
 
 
-
-func createExerciseIngressPolicy(clientSet kubernetes.Clientset, teamName string, podName string) {
+func createKaliEgressPolicy(clientSet kubernetes.Clientset, teamName string, podName string) {
+	//necessary to get address of a constant: https://stackoverflow.com/questions/35146286/find-address-of-constant-in-go
+        getAddress := func(s v1.Protocol) *v1.Protocol { return &s }
 	policyTypes := []network.PolicyType {}
 	ingress := []network.NetworkPolicyIngressRule {} //empty rule because we need the param and nil might screw us but empty rule might 
 	//also screw us as all ingress might just be denied...
 	egress := []network.NetworkPolicyEgressRule {
 		{
-			To: []network.NetworkPolicyPeer{}, //fill me 
-			Ports: []network.NetworkPolicyPort{}, //fill me
+			
+			To: []network.NetworkPolicyPeer{
+				{       //this is for allowing internal dns 
+					NamespaceSelector: &metav1.LabelSelector{},
+					PodSelector: &metav1.LabelSelector{
+						MatchLabels: map[string]string{
+							"k8s-app": "kube-dns",
+						},
+					},
+				},
+				{
+					//here goes the actual "type: exercise" and namespace selection 
+					//according to docs: no namespace selection means "pick own namespace" -> cool!
+					//https://pkg.go.dev/k8s.io/api/networking/v1#NetworkPolicySpec
+				},
+
+			},
+			Ports: []network.NetworkPolicyPort{
+				{
+					Port: &intstr.IntOrString{Type: intstr.Type(intstr.Int), IntVal: 53},
+					Protocol: getAddress(v1.ProtocolUDP),
+				},
+
+			},
 		},
 	} 
-
 	createNetworkPolicy(clientSet, podName, "remember to handle podlabel", teamName, policyTypes, ingress, egress)
-
 }
-func createKaliEgressPolicy() {
-	fmt.Println("Hello World")
+
+//port limiting shouldnt be necessary on ingress as exercises only expose the ports they need to
+func createExerciseIngressPolicy(clientSet kubernetes.Clientset, teamName string, podName string) {
+	policyTypes := []network.PolicyType {}
+	egress := []network.NetworkPolicyEgressRule {} //empty rule because we need the param and nil might screw us but empty rule might 
+	//also screw us as all egress might just be denied...
+	ingress := []network.NetworkPolicyIngressRule {
+		{
+			From: []network.NetworkPolicyPeer{
+				{
+					//just deleting namespaceselector might be ok, see comment in above function 
+					NamespaceSelector: &metav1.LabelSelector{},
+					PodSelector: &metav1.LabelSelector{
+						MatchLabels: map[string]string{
+							"app": "vnc", //needs to align with kali labels
+						},
+					},
+				},
+			},
+		},
+	} 
+	createNetworkPolicy(clientSet, podName, "remember to handle podlabel", teamName, policyTypes, ingress, egress)
 }
 
 //demo
