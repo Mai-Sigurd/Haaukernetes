9e06e73908ec56e90d6c16c849a05c9cb9443655
kaep <sebastianholmgaard@live.dk>
2023-03-08 10:13:59 +0100

    make changes necessary for network policies to work. Now egress from kali is only allowed to exercises and ingress to exercises is only allowed from kali. This involves duplicating some of the existing code but done > perfect was the mindset here.

--- deployments/deployment.go
+++ deployments/deployment.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	utils "k8-project/utils"
+
 	appsv1 "k8s.io/api/apps/v1"
 	apiv1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -20,10 +21,20 @@ import (
 	// _ "k8s.io/client-go/plugin/pkg/client/auth/oidc"
 )
 
-// CreateDeployment configures a deployment and then creates a deployment from that configuration
+func CreateKaliDeployment(clientSet kubernetes.Clientset, teamName string, podName string, containerPort int32) {
+	deployment := configureKaliDeployment(teamName, podName, containerPort)
+	fmt.Printf("Creating kali deployment %s\n", deployment.ObjectMeta.Name)
+	deploymentsClient := clientSet.AppsV1().Deployments(teamName)
+	result, err := deploymentsClient.Create(context.TODO(), &deployment, metav1.CreateOptions{})
+	utils.ErrHandler(err)
+	fmt.Printf("Created kali deployment %q.\n", result.GetObjectMeta().GetName())
+
+}
+
+// CreateExerciseDeployment configures a deployment and then creates a deployment from that configuration
 // in the given namespace.
-func CreateDeployment(clientSet kubernetes.Clientset, teamName string, exerciseName string, containerPort int32) {
-	deployment := configureDeployment(teamName, exerciseName, containerPort)
+func CreateExerciseDeployment(clientSet kubernetes.Clientset, teamName string, exerciseName string, containerPort int32) {
+	deployment := configureExerciseDeployment(teamName, exerciseName, containerPort)
 	fmt.Printf("Creating deployment %s\n", deployment.ObjectMeta.Name)
 	deploymentsClient := clientSet.AppsV1().Deployments(teamName)
 	result, err := deploymentsClient.Create(context.TODO(), &deployment, metav1.CreateOptions{})
@@ -31,8 +42,7 @@ func CreateDeployment(clientSet kubernetes.Clientset, teamName string, exerciseN
 	fmt.Printf("Created deployment %q.\n", result.GetObjectMeta().GetName())
 }
 
-// configureDeployment makes a deployment configuration for a pod and replicaset
-func configureDeployment(nameSpace string, name string, containerPort int32) appsv1.Deployment {
+func configureKaliDeployment(nameSpace string, name string, containerPort int32) appsv1.Deployment {
 	deployment := &appsv1.Deployment{
 		ObjectMeta: metav1.ObjectMeta{
 			Name: name,
@@ -52,6 +62,52 @@ func configureDeployment(nameSpace string, name string, containerPort int32) app
 				ObjectMeta: metav1.ObjectMeta{
 					Labels: map[string]string{
 						"app": name,
+					},
+				},
+				Spec: apiv1.PodSpec{
+					Containers: []apiv1.Container{
+						{
+							Name:            name,
+							Image:           name,
+							ImagePullPolicy: apiv1.PullNever,
+							Ports: []apiv1.ContainerPort{
+								{
+									Name:          "http",
+									Protocol:      apiv1.ProtocolTCP,
+									ContainerPort: containerPort,
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+	return *deployment
+}
+
+// configureExerciseDeployment makes a deployment configuration for a pod and replicaset
+func configureExerciseDeployment(nameSpace string, name string, containerPort int32) appsv1.Deployment {
+	deployment := &appsv1.Deployment{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: name,
+			Labels: map[string]string{
+				"app": name,
+			},
+			Namespace: nameSpace, //NAMESPACE SKAL EKSISTERE OG MATCHE NAMESPACE I DEPLOYMENTKLIENTEN
+		},
+		Spec: appsv1.DeploymentSpec{
+			Replicas: utils.Int32Ptr(1),
+			Selector: &metav1.LabelSelector{
+				MatchLabels: map[string]string{
+					"app": name,
+				},
+			},
+			Template: apiv1.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{
+					Labels: map[string]string{
+						"app":  name,
+						"type": "exercise",
 					},
 				},
 				Spec: apiv1.PodSpec{
