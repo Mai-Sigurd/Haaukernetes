4203f79a554cfcdbe729a0c40b3be25c7d500cc8
Thea Kjeldsmark <tkjeldsmark@gmail.com>
2023-03-22 12:24:04 +0100

    Mere script

--- scripts/server-setup.sh
+++ scripts/server-setup.sh
@@ -1,1 +1,12 @@
-#!/bin/sh
+#!/bin/bash
+
+valid_ip=false
+
+while ! $valid_ip ; do
+  echo "Input control-plane server ip:"
+  read -r server_ip
+  if [[ $server_ip =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+    valid_ip=true
+  fi
+done
+
