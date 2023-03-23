abcca2238ad5c80eb59cdae9f400fb4b01d7d8c7
kaep <sebastianholmgaard@live.dk>
2023-03-23 20:16:01 +0100

    restructure + bug fix. vpn through code now works. netpols still not implemented. automatic insertion of nodeport in client-conf also not implemented yet

--- secrets/secrets.go
+++ secrets/secrets.go
@@ -10,14 +10,9 @@ import (
 	"k8s.io/client-go/kubernetes"
 )
 
-//vil det være federe hvis CreateSecret bare tager en secret som er præconf? så skal den ikke tage 1000 args...
-//Men det stemmer jo ikke overens med måden det er sat op de andre steder..
-//det er lækkert nok at struct bygges i configure ift. at alle funktionerne ikke bliver kæmpestore, men omvendt
-//så kan configuresecret jo med tiden ende med at tage 7000 args fordi der er forskellige usecases... det er
-//lidt et tradeoff uden et klart svar..
 func CreateWireGuardSecret(clientSet kubernetes.Clientset, teamName string, privatekey string) {
 	data := make(map[string][]byte)
-	data["privateKey"] = []byte(privatekey)
+	data["privatekey"] = []byte(privatekey)
 	secret := configureSecret("wg-secret", teamName, v1.SecretTypeOpaque, data)
 	CreateSecret(clientSet, teamName, secret)
 }
@@ -44,13 +39,3 @@ func configureSecret(name string, namespace string, secretType v1.SecretType, da
 
 	return *secret
 }
-
-//denne følger "konventionen" fulgt i de andre filer...
-//men det skal nok laves om / suppleres af den anden måde at lave create, hvor struct til create blot gives som param
-// func dep_CreateSecret(clientSet kubernetes.Clientset, teamName string) {
-// 	secret := configureSecret(teamName) //PLACEHOLDER, use
-// 	secretsClient := clientSet.CoreV1().Secrets(teamName)
-// 	result, err := secretsClient.Create(context.TODO(), &secret, metav1.CreateOptions{})
-// 	utils.ErrHandler(err)
-// 	fmt.Printf("Created secret %q.\n", result.GetObjectMeta().GetName())
-// }
