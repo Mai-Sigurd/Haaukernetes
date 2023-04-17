91527d15cf9f19730deed6769e0eb7da472fef65
Mai_Sigurd <odgaardmai@gmail.com>
2023-04-17 11:44:58 +0200

    print

--- tests/resource_test.go
+++ tests/resource_test.go
@@ -94,7 +94,7 @@ func logCPUContiously(c chan string) {
 		//actualCPU := float64(cpuNow.User) / float64(cpuNow.Total) * 100
 		actualCPU, _ := cpu.Percent(500*time.Millisecond, false)
 		thing := fmt.Sprintf("%f\n", actualCPU[0])
-		log.Printf(thing)
+		log.Print(thing)
 	}
 }
 
