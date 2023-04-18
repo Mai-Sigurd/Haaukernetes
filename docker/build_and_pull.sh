9d8b11b1aa0eec7d057509bff1d96232892adc36
kaep <sebastianholmgaard@live.dk>
2023-04-18 11:23:55 +0200

    remove deprecated docker dir, as we no longer use it

--- docker/build_and_pull.sh
+++
@@ -1,4 +0,0 @@
-docker build -f ./admin_logon/Dockerfile -t benis_logon ./admin_logon
-docker build -f ./heartbleed/Dockerfile -t benis_heartbleed ./heartbleed 
-docker build -f ./kali_dockerfile -t benis_kali-vnc . 
-docker pull masipcat/wireguard:latest -t benis_wireguard
