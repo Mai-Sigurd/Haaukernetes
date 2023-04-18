4df8513d01d97660d6c7c97da09c8872aa92219d
Thea Kjeldsmark <tkjeldsmark@gmail.com>
2023-04-18 15:30:50 +0200

    Restructure main with logs to utils and router func

---
+++ utils/setLog.go
@@ -0,0 +1,17 @@
+package utils
+
+import (
+	"log"
+	"os"
+	"time"
+)
+
+func SetLog() {
+	currentTime := time.Now()
+	f, err := os.OpenFile(currentTime.Format("2006.01.02 15:04:05"), os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatalf("error opening file: %v", err)
+	}
+	defer f.Close()
+	log.SetOutput(f)
+}
