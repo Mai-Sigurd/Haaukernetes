84504522b2e8764197594751edb9888114beaf4c
Thea Kjeldsmark <tkjeldsmark@gmail.com>
2023-04-18 15:34:45 +0200

    Make logs go into logs folder instead of the root directory

--- utils/setLog.go
+++ utils/setLog.go
@@ -8,7 +8,7 @@ import (
 
 func SetLog() {
 	currentTime := time.Now()
-	f, err := os.OpenFile(currentTime.Format("2006.01.02 15:04:05"), os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	f, err := os.OpenFile("logs/"+currentTime.Format("2006.01.02 15:04:05"), os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
 	if err != nil {
 		log.Fatalf("error opening file: %v", err)
 	}
