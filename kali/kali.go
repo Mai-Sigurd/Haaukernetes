3819a02e76d2954f789a74294873fa300f8419e5
kaep <sebastianholmgaard@live.dk>
2023-04-19 12:51:46 +0200

    remove newly added return of kali socket address as it will not be needed because of guacamole. also change kali struct and remove minikube related comment

--- kali/kali.go
+++ kali/kali.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"k8-project/deployments"
 	"k8-project/services"
-	"strconv"
 
 	"k8s.io/client-go/kubernetes"
 )
@@ -12,13 +11,11 @@ import (
 const kaliRDPPort = 13389
 const kaliImageName = "kali"
 
-func StartKali(clientSet kubernetes.Clientset, namespace string) string {
+func StartKali(clientSet kubernetes.Clientset, namespace string) {
 	fmt.Println("Starting Kali")
 	podLabels := make(map[string]string)
 	podLabels["app"] = "kali"
 	ports := []int32{kaliRDPPort}
 	deployments.CreateDeployment(clientSet, namespace, "kali", kaliImageName, ports, podLabels)
-	service := services.CreateService(clientSet, namespace, "kali", ports)
-	kaliSocketAddress := service.Spec.ClusterIP + ":" + strconv.Itoa(kaliRDPPort)
-	return kaliSocketAddress
+	services.CreateService(clientSet, namespace, "kali", ports)
 }
