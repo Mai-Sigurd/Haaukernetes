bab4b414ea6117a35a9ba1a4c40c4ac9658f297a
Thea Kjeldsmark <tkjeldsmark@gmail.com>
2023-04-26 14:58:49 +0200

    Change scripts

--- scripts/install-k8s/control-plane-setup.sh
+++ scripts/install-k8s/control-plane-setup.sh
@@ -2,26 +2,28 @@
 
 set -e
 
-SERVER_IP=""
 NODENAME=$(hostname -s)
-POD_CIDR="192.168.0.0/16"
 
 [[ $EUID -ne 0 ]] && echo "This script must be run as root" && exit 1
 echo "##### Starting control-plane setup"
 chmod +x common-setup.sh
 
-while true; do
-  echo "Input control-plane server ip:"
-  read -r SERVER_IP
-  if [[ $SERVER_IP =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
-    break
-  fi
-done
+#while true; do
+#  echo "Input control-plane server ip:"
+#  read -r SERVER_IP
+#  if [[ $SERVER_IP =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+#    break
+#  fi
+#done
 
 ./common-setup.sh
 
+PUBLIC_IP=$(ip -f inet -o addr show eth0|cut -d\  -f 7 | cut -d/ -f 1 | head -n 1)
+
+# CIDR="${SERVER_IP}/16"
+
 echo "##### Initializing Kubeadm on control-plane node"
-kubeadm init --apiserver-advertise-address=$IPADDR  --apiserver-cert-extra-sans=$IPADDR  --pod-network-cidr=$POD_CIDR --node-name $NODENAME --ignore-preflight-errors Swap
+kubeadm init --apiserver-advertise-address=$PUBLIC_IP  --pod-network-cidr=192.168.0.0/16 --node-name $NODENAME --ignore-preflight-errors Swap
 
 echo "##### Creating kubeconfig file"
 mkdir -p $HOME/.kube
