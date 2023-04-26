cb34e3f5544d04e8b6bde3f68a6d2309a24bc437
Thea Kjeldsmark <tkjeldsmark@gmail.com>
2023-04-26 22:48:47 +0200

    Der sker noget

--- guacamole/postgres/init.sh
+++ guacamole/postgres/init.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# kubectl logs guacamole-7f86c55467-24442 -c guacamole -n guacamole
+# Kig lige lidt på versions i de andre filer
+# sammenlign med https://oopflow.medium.com/how-to-install-guacamole-on-kubernetes-7d747438c141
+
 # Exit if any command fails
 set -e
 
@@ -11,6 +15,7 @@ echo ""
 echo "Creating guacamole namespace"
 kubectl apply -f guacamole-namespace.yaml
 
+# FIXX SÆT hostname til inside IP på postgres
 echo "Creating K8 db secret"
 kubectl create secret generic postgres \
     --from-literal postgres-user=guacamole \
@@ -28,3 +33,14 @@ kubectl apply -f postgres-deployment.yaml
 
 echo "Create postgres service"
 kubectl apply -f postgres-service.yaml
+
+
+# FIXX POD NAME ETC
+kubectl exec -it postgres-7996cd45c5-7qpsw -n guacamole -- psql -h localhost -d guacamole -U guacamole -p 5432 < initdb.sql
+
+
+echo "Creating guacamole deployment"
+kubectl apply -f guacamole-deployment.yaml
+
+echo "Creating guacamole service"
+kubectl apply -f guacamole-service.yaml
