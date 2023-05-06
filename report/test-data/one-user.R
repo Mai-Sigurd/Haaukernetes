9e047957eb933294f04e250e4eb18a9232621823
Thea Kjeldsmark <tkjeldsmark@gmail.com>
2023-05-06 13:54:47 +0200

    Add more test data, graphs, update R code

--- report/test-data/one-user.R
+++ report/test-data/one-user.R
@@ -1,28 +1,31 @@
 # install.packages("ggplot2")
 library(ggplot2)
 
+plot <- "mem"
 home_path <- "/Users/theakjeldsmark/Outside OneDrive/"
 test_type <- "wireguard"
-chals <- 6 # 6 or 30
+chals <- 30 # 5 or 30
 test <- paste0("test", chals, "challenges", test_type, "oneuser")
-path <- paste0(home_path, "Haaukernetes/report/test-data/data/one-user/", test, "/")
-test_name <- paste0("one ", test_type, " user with ", chals, " challenges")
+path <- paste0(home_path,
+         "Haaukernetes/report/test-data/data/one-user/",
+         test,
+         "/")
 
 s <- 180
 
 # CPU ----------------------------------------------------------------------
 
-# Create an empty data frame with seconds 1 to 180
+# Create an empty data frame with seconds 1 to s
 df <- data.frame(seconds = seq(1, s))
 
 # Create an empty data frame to store max CPU values
-max_cpu_df <- data.frame(seconds = numeric(), max_cpu = numeric()) 
+max_cpu_df <- data.frame(seconds = numeric(), max_cpu = numeric())
 
 # Loop through the files and add the CPU column to the data frame
 for (i in 1:5) {
   file_name <- paste0(path, "cpu-", i, ".csv")
   cpu_data <- read.csv(file_name)
-  cpu_col <- cpu_data[, 2][1:s] # Extract second column and first 180 rows
+  cpu_col <- cpu_data[, 2][1:s]
   df <- cbind(df, cpu_col)
   
   max_cpu <- max(cpu_col) # Get the max CPU value
@@ -35,44 +38,51 @@ for (i in 1:5) {
 max_cpu_df
 
 # Add columns with average, minimum, and maximum values
-df$minimum <- apply(df[, -1], 1, min) # Exclude seconds column from calculations
-df$mean <- rowMeans(df[, -1])
-df$maximum <- apply(df[, -1], 1, max)
+df$minimum <- apply(df[,-1], 1, min) # Exclude seconds column from calculations
+df$mean <- rowMeans(df[,-1])
+df$maximum <- apply(df[,-1], 1, max)
 
 # Rename the columns
-colnames(df) <- c("seconds", "cpu-1", "cpu-2", "cpu-3", "cpu-4", "cpu-5", "minimum", "mean", "maximum")
+colnames(df) <-
+  c("seconds",
+    "cpu-1",
+    "cpu-2",
+    "cpu-3",
+    "cpu-4",
+    "cpu-5",
+    "minimum",
+    "mean",
+    "maximum")
 
 # Plot
-ggplot(df, aes(x = seconds)) +
+cpu_plot <- ggplot(df, aes(x = seconds)) +
   geom_line(aes(y = `cpu-1`, color = "CPU-1")) +
   geom_line(aes(y = `cpu-2`, color = "CPU-2")) +
   geom_line(aes(y = `cpu-3`, color = "CPU-3")) +
   geom_line(aes(y = `cpu-4`, color = "CPU-4")) +
   geom_line(aes(y = `cpu-5`, color = "CPU-5")) +
   xlab("Time Elapsed (s)") +
-  ylab("CPU") +
-  ggtitle(paste("Namespace CPU usage over time: ", test_name, sep="")) +
-  theme(plot.title = element_text(hjust = 0.5)) +
-  scale_color_manual(values = c("red", "green", "blue", "purple", "orange"), 
-                     name = "Test Runs:", 
-                     labels = c("1", "2", "3", "4", "5")) + 
+  ylab("CPUs") +
+  scale_color_manual(
+    values = c("red", "green", "blue", "purple", "orange"),
+    name = "Test Runs:",
+    labels = c("1", "2", "3", "4", "5")
+  ) +
   scale_x_continuous(breaks = seq(0, s, 20), limits = c(0, s)) +
-  scale_y_continuous(breaks = seq(0, 0.175, 0.025), limits = c(0, 0.175)) +
+  scale_y_continuous(breaks = seq(0, 0.055, 0.0125), limits = c(0, 0.055)) +
   theme_bw() +
-  theme(plot.title = element_text(hjust = 0.5, size = 14), 
-        plot.title.position = "plot", legend.position="bottom")
+  theme(legend.position = "bottom", text = element_text(size = 14))
 
 
 
 
 # Memory ----------------------------------------------------------------------
-s <- 180
 
-# Create an empty data frame with seconds 1 to 180
+# Create an empty data frame with seconds 1 to s
 df <- data.frame(seconds = seq(1, s))
 
 # Create an empty data frame to store max memory values
-max_mem_df <- data.frame(seconds = numeric(), max_mem = numeric()) 
+max_mem_df <- data.frame(seconds = numeric(), max_mem = numeric())
 
 # Loop through the files and add the memory column to the data frame
 for (i in 1:5) {
@@ -80,7 +90,7 @@ for (i in 1:5) {
   mem_data <- read.csv(file_name)
   mem_col <- mem_data[, 2][1:s] / 1048576 # Convert bytes to mebibytes
   df <- cbind(df, mem_col)
-
+  
   max_mem <- max(mem_col) # Get the max memory value
   max_mem_seconds <- df$seconds[which.max(mem_col)] # Get the corresponding seconds
   
@@ -91,15 +101,24 @@ for (i in 1:5) {
 max_mem_df
 
 # Add columns with average, minimum, and maximum values
-df$minimum <- apply(df[, -1], 1, min) # Exclude seconds column from calculations
-df$mean <- rowMeans(df[, -1])
-df$maximum <- apply(df[, -1], 1, max)
+df$minimum <- apply(df[,-1], 1, min) # Exclude seconds column from calculations
+df$mean <- rowMeans(df[,-1])
+df$maximum <- apply(df[,-1], 1, max)
 
 # Rename the columns
-colnames(df) <- c("seconds", "mem-1", "mem-2", "mem-3", "mem-4", "mem-5", "minimum", "mean", "maximum")
+colnames(df) <-
+  c("seconds",
+    "mem-1",
+    "mem-2",
+    "mem-3",
+    "mem-4",
+    "mem-5",
+    "minimum",
+    "mean",
+    "maximum")
 
 # Plot
-ggplot(df, aes(x = seconds)) +
+mem_plot <- ggplot(df, aes(x = seconds)) +
   geom_line(aes(y = `mem-1`, color = "mem-1")) +
   geom_line(aes(y = `mem-2`, color = "mem-2")) +
   geom_line(aes(y = `mem-3`, color = "mem-3")) +
@@ -107,22 +126,20 @@ ggplot(df, aes(x = seconds)) +
   geom_line(aes(y = `mem-5`, color = "mem-5")) +
   xlab("Time Elapsed (s)") +
   ylab("Memory (MiB)") +
-  ggtitle(paste("Namespace memory usage over time: ", test_name, sep="")) +
-  theme(plot.title = element_text(hjust = 0.5)) +
-  scale_color_manual(values = c("red", "green", "blue", "purple", "orange"), 
-                     name = "Test Runs:", 
-                     labels = c("1", "2", "3", "4", "5")) + 
-  #scale_x_continuous(breaks = seq(0, s, 20), limits = c(0, s)) +
-  #scale_y_continuous(breaks = seq(0, 50, 10), limits = c(0, 50)) +
+  scale_color_manual(
+    values = c("red", "green", "blue", "purple", "orange"),
+    name = "Test Runs:",
+    labels = c("1", "2", "3", "4", "5")
+  ) +
+  scale_x_continuous(breaks = seq(0, s, 20), limits = c(0, s)) +
+  scale_y_continuous(breaks = seq(0, 275, 50), limits = c(0, 275)) +
   theme_bw() +
-  theme(plot.title = element_text(hjust = 0.5, size = 14), 
-        plot.title.position = "plot", legend.position="bottom")
+  theme(legend.position = "bottom", text = element_text(size = 14))
 
 
-
-
-
-
-
-
+if (plot == "cpu") {
+  cpu_plot
+} else {
+  mem_plot
+}
 
--- report/test-data/several-users.R
+++ report/test-data/several-users.R
@@ -1,28 +1,30 @@
 # install.packages("ggplot2")
 library(ggplot2)
 
+plot <- "mem"
 home_path <- "/Users/theakjeldsmark/Outside OneDrive/"
-test <- "test8Namespaces"
-# test <- "test16Namespaces"
-path <- paste0(home_path, "Haaukernetes/report/test-data/data/several-users/", test, "/")
-test_name <- "8 namespaces with 5 challenges"
+test <- "test16Namespaces"
+path <- paste0(home_path,
+         "Haaukernetes/report/test-data/data/several-users/",
+         test,
+         "/")
 
-s <- 180
+s <- 300
 
 
 # CPU ----------------------------------------------------------------------
 
-# Create an empty data frame with seconds 1 to 180
+# Create an empty data frame with seconds 1 to s
 df <- data.frame(seconds = seq(1, s))
 
 # Create an empty data frame to store max CPU values
-max_cpu_df <- data.frame(seconds = numeric(), max_cpu = numeric()) 
+max_cpu_df <- data.frame(seconds = numeric(), max_cpu = numeric())
 
 # Loop through the files and add the CPU column to the data frame
 for (i in 1:5) {
   file_name <- paste0(path, "cpu-", i, ".csv")
   cpu_data <- read.csv(file_name)
-  cpu_col <- cpu_data[, 2][1:s] # Extract second column and first 180 rows
+  cpu_col <- cpu_data[, 2][1:s]
   cpu_col_subtracted <- 100 - cpu_col # Subtract 100 from each element since we are read "idle"
   df <- cbind(df, cpu_col_subtracted)
   
@@ -36,15 +38,24 @@ for (i in 1:5) {
 max_cpu_df
 
 # Add columns with average, minimum, and maximum values
-df$minimum <- apply(df[, -1], 1, min) # Exclude seconds column from calculations
-df$mean <- rowMeans(df[, -1])
-df$maximum <- apply(df[, -1], 1, max)
+df$minimum <- apply(df[,-1], 1, min) # Exclude seconds column from calculations
+df$mean <- rowMeans(df[,-1])
+df$maximum <- apply(df[,-1], 1, max)
 
 # Rename the columns
-colnames(df) <- c("seconds", "cpu-1", "cpu-2", "cpu-3", "cpu-4", "cpu-5", "minimum", "mean", "maximum")
+colnames(df) <-
+  c("seconds",
+    "cpu-1",
+    "cpu-2",
+    "cpu-3",
+    "cpu-4",
+    "cpu-5",
+    "minimum",
+    "mean",
+    "maximum")
 
 # Plot
-ggplot(df, aes(x = seconds)) +
+cpu_plot <- ggplot(df, aes(x = seconds)) +
   geom_line(aes(y = `cpu-1`, color = "CPU-1")) +
   geom_line(aes(y = `cpu-2`, color = "CPU-2")) +
   geom_line(aes(y = `cpu-3`, color = "CPU-3")) +
@@ -52,28 +63,26 @@ ggplot(df, aes(x = seconds)) +
   geom_line(aes(y = `cpu-5`, color = "CPU-5")) +
   xlab("Time Elapsed (s)") +
   ylab("CPU (%)") +
-  ggtitle(paste("Node CPU usage over time: ", test_name, sep="")) +
-  theme(plot.title = element_text(hjust = 0.5)) +
-  scale_color_manual(values = c("red", "green", "blue", "purple", "orange"), 
-                     name = "Test Runs:", 
-                     labels = c("1", "2", "3", "4", "5")) + 
-  scale_x_continuous(breaks = seq(0, s, 20), limits = c(0, s)) +
-  scale_y_continuous(breaks = seq(0, 35, 5), limits = c(0, 35)) +
+  scale_color_manual(
+    values = c("red", "green", "blue", "purple", "orange"),
+    name = "Test Runs:",
+    labels = c("1", "2", "3", "4", "5")
+  ) +
+  scale_x_continuous(breaks = seq(0, s, 30), limits = c(0, s)) +
+  scale_y_continuous(breaks = seq(0, 55, 10), limits = c(0, 55)) +
   theme_bw() +
-  theme(plot.title = element_text(hjust = 0.5, size = 14), 
-        plot.title.position = "plot", legend.position="bottom")
+  theme(legend.position = "bottom", text = element_text(size = 14))
 
 
 
 
 # Memory ----------------------------------------------------------------------
-s <- 180
 
-# Create an empty data frame with seconds 1 to 180
+# Create an empty data frame with seconds 1 to s
 df <- data.frame(seconds = seq(1, s))
 
 # Create an empty data frame to store max memory values
-max_mem_df <- data.frame(seconds = numeric(), max_mem = numeric()) 
+max_mem_df <- data.frame(seconds = numeric(), max_mem = numeric())
 
 # Loop through the files and add the memory column to the data frame
 for (i in 1:5) {
@@ -92,15 +101,24 @@ for (i in 1:5) {
 max_mem_df
 
 # Add columns with average, minimum, and maximum values
-df$minimum <- apply(df[, -1], 1, min) # Exclude seconds column from calculations
-df$mean <- rowMeans(df[, -1])
-df$maximum <- apply(df[, -1], 1, max)
+df$minimum <- apply(df[,-1], 1, min) # Exclude seconds column from calculations
+df$mean <- rowMeans(df[,-1])
+df$maximum <- apply(df[,-1], 1, max)
 
 # Rename the columns
-colnames(df) <- c("seconds", "mem-1", "mem-2", "mem-3", "mem-4", "mem-5", "minimum", "mean", "maximum")
+colnames(df) <-
+  c("seconds",
+    "mem-1",
+    "mem-2",
+    "mem-3",
+    "mem-4",
+    "mem-5",
+    "minimum",
+    "mean",
+    "maximum")
 
 # Plot
-ggplot(df, aes(x = seconds)) +
+mem_plot <- ggplot(df, aes(x = seconds)) +
   geom_line(aes(y = `mem-1`, color = "mem-1")) +
   geom_line(aes(y = `mem-2`, color = "mem-2")) +
   geom_line(aes(y = `mem-3`, color = "mem-3")) +
@@ -108,22 +126,19 @@ ggplot(df, aes(x = seconds)) +
   geom_line(aes(y = `mem-5`, color = "mem-5")) +
   xlab("Time Elapsed (s)") +
   ylab("Memory (MiB)") +
-  ggtitle(paste("Node memory usage over time: ", test_name, sep="")) +
-  theme(plot.title = element_text(hjust = 0.5)) +
-  scale_color_manual(values = c("red", "green", "blue", "purple", "orange"), 
-                     name = "Test Runs:", 
-                     labels = c("1", "2", "3", "4", "5")) + 
-  scale_x_continuous(breaks = seq(0, s, 20), limits = c(0, s)) +
-  scale_y_continuous(breaks = seq(1000, 3000, 500), limits = c(1000, 3000)) +
+  scale_color_manual(
+    values = c("red", "green", "blue", "purple", "orange"),
+    name = "Test Runs:",
+    labels = c("1", "2", "3", "4", "5")
+  ) +
+  scale_x_continuous(breaks = seq(0, s, 30), limits = c(0, s)) +
+  scale_y_continuous(breaks = seq(1000, 4600, 500), limits = c(1000, 4600)) +
   theme_bw() +
-  theme(plot.title = element_text(hjust = 0.5, size = 14), 
-        plot.title.position = "plot", legend.position="bottom")
+  theme(legend.position = "bottom", text = element_text(size = 14))
 
 
-
-
-
-
-
-
-
+if (plot == "cpu") {
+  cpu_plot
+} else {
+  mem_plot
+}
